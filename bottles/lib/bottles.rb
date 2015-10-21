08e84db811d388226b92646eda84ba288a2865e6
Sandi Metz <sandi@sandimetz.com>
2015-10-21 08:35:11 -0400

    extracted BottleNumber

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -11,10 +11,10 @@ class Bottles
   def verse(number)
     bottle_number      = BottleNumber.new(number)
     next_bottle_number = BottleNumber.new(bottle_number.successor)
-    "#{bottle_number.amount.capitalize} #{bottle_number.container} of beer on the wall, " +
-    "#{bottle_number.amount} #{bottle_number.container} of beer.\n" +
+    "#{bottle_number} of beer on the wall, ".capitalize +
+    "#{bottle_number} of beer.\n" +
     "#{bottle_number.action}, " +
-    "#{next_bottle_number.amount} #{next_bottle_number.container} of beer on the wall.\n"
+    "#{next_bottle_number} of beer on the wall.\n"
   end
 end
 
@@ -23,6 +23,18 @@ class BottleNumber
 
   def initialize(number)
     @number = number
+  end
+
+  def to_s
+    "#{amount} #{container}"
+  end
+
+  def amount
+    if number == 0
+      "no more"
+    else
+      number.to_s
+    end
   end
 
   def container
@@ -38,14 +50,6 @@ class BottleNumber
       "it"
     else
       "one"
-    end
-  end
-
-  def amount
-    if number == 0
-      "no more"
-    else
-      number.to_s
     end
   end
 
